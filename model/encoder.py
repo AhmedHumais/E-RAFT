56181b1300297ce42e8c551b10899b46b038ab7d
HussainMSajwani <hussainmsajwani@gmail.com>
2023-02-11 22:43:37 +0400

    used torch.nn.BatchNorm1d instead of torch_geometric.nn.BatchNorm

--- model/encoder.py
+++ model/encoder.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch_geometric.nn import SplineConv
 from torch_geometric.transforms import Cartesian
 from .maxpooling import MaxPooling, MaxPoolingX, MaxPooling2
-
+from torch_geometric.nn.norm import BatchNorm
 # TODO: find the difference between batchnorm and instance norm and if we need it for context and feature networks
 
 class GraphEncoder(nn.Module):
@@ -17,25 +17,25 @@ class GraphEncoder(nn.Module):
         pseudo = Cartesian(norm=True, cat=False)
 
         self.conv1 = SplineConv(n_feature, 32, dim=3, kernel_size=2)
-        self.norm1 = nn.BatchNorm1d(32)
+        self.norm1 = BatchNorm(32)
 
         self.conv2 = SplineConv(32, 64, dim=3, kernel_size=2)
-        self.norm2 = nn.BatchNorm1d(64)
+        self.norm2 = BatchNorm(64)
         self.pool2 = MaxPooling2(2, transform=pseudo)
 
         self.conv3 = SplineConv(64, 64, dim=3, kernel_size=2)
-        self.norm3 = nn.BatchNorm1d(64)
+        self.norm3 = BatchNorm(64)
         self.pool3 = MaxPooling2(2, transform=pseudo)
         
         self.conv4 = SplineConv(64, 64, dim=3, kernel_size=2)
-        self.norm4 = nn.BatchNorm1d(64)
+        self.norm4 = BatchNorm(64)
         self.pool4 = MaxPooling2(2, transform=pseudo)
 
         self.conv5 = SplineConv(64, 128, dim=3, kernel_size=2)
-        self.norm5 = nn.BatchNorm1d(128)
+        self.norm5 = BatchNorm(128)
 
         self.conv6 = SplineConv(128, output_dim, dim=3, kernel_size=2)
-        self.norm6 = nn.BatchNorm1d(output_dim)
+        self.norm6 = BatchNorm(output_dim)
         
 
     def forward(self, in_data):
